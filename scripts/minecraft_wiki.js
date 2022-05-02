a740404ae8994ed218c46e77ef18a93640090e70
extclp <2842417407@qq.com>
2022-05-03 01:01:48 +0800

    fisrt commit

---
+++ scripts/minecraft_wiki.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Minecraft Wiki界面优化
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       extclp
+// @match        https://minecraft.fandom.com/*
+// @icon         https://static.wikia.nocookie.net/minecraft_gamepedia/images/4/4a/Site-favicon.ico
+// @grant        GM_addStyle
+// @run-at       document-start
+// ==/UserScript==
+
+GM_addStyle(
+  ".main-container{width:100%;margin:auto}.global-navigation,.fandom-sticky-header,.global-footer,.mcf-wrapper{display:none}"
+);
