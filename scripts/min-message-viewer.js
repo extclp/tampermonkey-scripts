8d5d2bc546eb68002af09f90d107e9b633a4c436
extclp <2842417407@qq.com>
2022-11-17 03:40:42 +0800

    MinMessageViewer 界面优化

---
+++ scripts/min-message-viewer.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         MinMessageViewer 界面优化
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       extclp
+// @match        https://webui.adventure.kyori.net/
+// @icon         https://webui.adventure.kyori.net/favicon-32x32.png
+// @grant        GM_addStyle
+// @run-at       document-start
+// ==/UserScript==
+
+GM_addStyle("html{overflow: auto;}");
